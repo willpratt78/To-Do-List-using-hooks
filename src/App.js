15e6cfa05078bcce534e676053fdad5d379ea246
William Pratt <willpratt78@gmail.com>
2022-07-06 16:52:48 -0700

    finshed handleToggle

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Header from "./Header";
+import data from "./data.json"
+import ToDoList from './ToDoList';
 
 function App() {
+  const [ toDoList, setToDoList ] = useState(data);
+
+
+  const handleToggle = (id) => {
+    let mapped = toDoList.map(task => {
+      return task.id = id ? {...task, complete: !task.complete } : { ...task};
+    });
+    setToDoList(mapped);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <ToDoList toDoList ={toDoList}/>
     </div>
   );
 }
