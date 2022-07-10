47a6d550ac6be611233681197922647ce10f3780
William Pratt <willpratt78@gmail.com>
2022-07-10 10:31:47 -0700

    functional ToDo List

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ToDo from './ToDo';
 
-const ToDoList = ({toDoList}) => {
+const ToDoList = ({toDoList, handleToggle, handleFilter}) => {
   return (
     <div>
       {toDoList.map(todo => {
         return (
-          <ToDo todo={todo} />
+          <ToDo todo={todo} handleToggle={handleToggle} handleFilter={handleFilter} />
         )
       })}
+      <button style={{margin: '20px'}} onClick={handleFilter}>Clear Completed Tasks</button>
     </div>
   );
 };
